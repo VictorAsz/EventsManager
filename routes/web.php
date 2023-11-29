b1fac6572b5d412e208df77158cd27cbf39aaa0c
Victor Assunção <victor.a.schumann@gmail.com>
2023-11-29 12:57:00 -0300

    add public folders

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $nome = "Matheus";
+    $idade = 29;
+    $arr = [1,2,3,4,5];
+    $nomes = ['matheus', 'Maria', 'João', 'Saulo'];
+    return view('welcome',  
+    [
+        'nome' => $nome,
+        'idade2' => $idade,
+        'profissao' => 'programador',
+        'arr' => $arr,
+        'nomes' => $nomes
+    ]
+);
 });
 Route::get('/contact', function () {
     return view(' contact');
