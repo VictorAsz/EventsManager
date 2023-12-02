68a0b5d9aad92d2290ba54b7ca96feb878aad693
Victor Assunção <victor.a.schumann@gmail.com>
2023-12-02 15:46:45 -0300

    restore code and add upload images, and rresources

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,49 +3,56 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <!-- Fonte do Google -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto">
-
-        <!-- CSS da aplicação -->
-         <link rel="stylesheet" href="/css/styles.css">
-        <!-- CSS Boostrap -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
         <title>@yield('title')</title>
-       
+
+        <!-- Fonte do Google -->
+        <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+
+        <!-- CSS Bootstrap -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+        <!-- CSS da aplicação -->
+        <link rel="stylesheet" href="/css/styles.css">
         <script src="/js/scripts.js"></script>
     </head>
     <body>
-        <header>
-            <nav class="navbar navbar-expand-lg navbar-light">
-                <div class="collapse navbar-collapse" id="navbar">
-                    <a href="/" class="navbar-brand">
-                        <img src="/img/hdcevents_logo.svg" alt="HDC Events">
-                    </a>    
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a href="/" class="nav-link">Eventos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/events/create" class="nav-link">Criar Eventos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/" class="nav-link">Entrar</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/" class="nav-link">Cadastrar</a>
-                        </li>
-                    </ul>
-                
-                </div>    
-            </nav>       
-        </header>
-
-         @yield('content')  
-        <footer>
-            <p>HDC Events &copy; 2023 </p>
-        </footer>
-        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+      <header>
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <div class="collapse navbar-collapse" id="navbar">
+            <a href="/" class="navbar-brand">
+              <img src="/img/hdcevents_logo.svg" alt="HDC Events">
+            </a>
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a href="/" class="nav-link">Eventos</a>
+              </li>
+              <li class="nav-item">
+                <a href="/events/create" class="nav-link">Criar Eventos</a>
+              </li>
+              <li class="nav-item">
+                <a href="/" class="nav-link">Entrar</a>
+              </li>
+              <li class="nav-item">
+                <a href="/" class="nav-link">Cadastrar</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <div class="container-fluid">
+          <div class="row">
+            @if(session('msg'))
+              <p class="msg">{{ session('msg') }}</p>
+            @endif
+            @yield('content')
+          </div>
+        </div>
+      </main>
+      <footer>
+        <p>HDC Events &copy; 2020</p>
+      </footer>
+      <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
     </body>
 </html>
