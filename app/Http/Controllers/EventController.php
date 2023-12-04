18e27ebdee5ea9b9c7b5b38b8d2034dce51280e1
Victor Assunção <victor.a.schumann@gmail.com>
2023-12-03 23:40:59 -0300

    Adicionado busca no site

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -9,10 +9,15 @@ use App\Models\Event;
 class EventController extends Controller{
 
     public function index() {
+        $search = request('search');
 
-        $events = Event::all();
-    
-        return view('welcome',['events' => $events]);
+        if($search){
+            $events = Event::where([
+                ['title', 'like', '%'.$search. '%']]) -> get();
+        }else{
+            $events = Event::all();
+        }
+        return view('welcome',['events' => $events, 'search' => $search]);
 
     }
     public function create() {
@@ -23,11 +28,12 @@ class EventController extends Controller{
 
         $event = new Event;
         $event->title = $request->title;
+        $event->date = $request ->date;
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
         $event->items = $request->items;
-        
+
           // Image Upload
           if($request->hasFile('image') && $request->file('image')->isValid()) {
 
